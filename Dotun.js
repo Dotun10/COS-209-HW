3cc0809e8dd81eb7baf4d23178f4f235be86b2d8
Dotun10 <121804662+Dotun10@users.noreply.github.com>
2024-12-04 14:29:53 +0100

    Add files via upload

---
+++ Dotun.js
@@ -0,0 +1,19 @@
+let student = {
+   'Name': 'Olamidotun David Komolafe',
+    Age: 92,
+   'Application ID': 'LS975620',
+   'Matric No': '22/7620' ,
+    School: 'Comupting & Engineering Sciences',
+    Course: 'Computer tECHNOLOGY',
+    Email: 'morahtochuckwu@gmail.com',
+    Nationality: 'Nigerian',
+      Address:  {
+       Street: 'Lekki phase 1 Oba street',
+       LGA: 'Ibeju Lekki',
+       State: 'Lagos',
+       Country: 'Nigeria'
+      }     
+                     
+   };
+ console.log(student)
+
